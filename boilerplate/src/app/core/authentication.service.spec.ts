1e97bf21a22236b262579f4334b9a34918629b43
Vanesa López <vlopezfe@ext.unicaja.es>
2020-01-30 15:24:15 +0100

    Fixed close

--- boilerplate/src/app/core/authentication.service.spec.ts
+++ boilerplate/src/app/core/authentication.service.spec.ts
@@ -6,10 +6,10 @@ xdescribe('AuthenticationService', () => {
 
   beforeEach(() => {
     beforeEach(() => TestBed.configureTestingModule({}));
+  });
 
   it('should be created', () => {
     const service: AuthenticationService = TestBed.get(AuthenticationService);
     expect(service).toBeTruthy();
   });
 });
-
